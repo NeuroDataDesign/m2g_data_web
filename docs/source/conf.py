67a6ea9e4fc9f3768022514170361ded3932ac7a
FangCai <54511589+FangCai-fifi@users.noreply.github.com>
2020-03-24 21:01:41 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,15 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 import sphinx_rtd_theme
-import numpydoc
-import sphinx_markdown
-import recommonmark
-import sphinx.ext.autodoc
 
 
 # -- Project information -----------------------------------------------------
@@ -34,7 +30,7 @@ author = 'Chuankai_Luo'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx_markdown', 'recommonmark']
+extensions = ['sphinx.ext.autodoc']
 
 html_copy_source = True
 
