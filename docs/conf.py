4d8dcccb832a9407da18275707647e23890bc911
Fang Cai <fangcai@Fangs-MacBook-Pro.local>
2020-03-30 14:02:15 -0400

    conf.py changes

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -12,24 +14,29 @@
 #
 import os
 import sys
+
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-
-import sphinx_rtd_theme
-
+sys.path.insert(0, os.path.abspath("../ndmg/"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'm2g_data'
-copyright = '2020, Chuankai and Fang'
-author = 'Chuankai and Fang'
+project = u'm2g'
+copyright = '2020, Gregory Kiar, Eric Bridgeford, Chuankai Luo, Fang Cai'
+author = u'Gregory Kiar, Eric Bridgeford, Chuankai Luo, Fang Cai'
+
+# The short X.Y version
+version = u''
+# The full version, including alpha/beta/rc tags
+release = u'0.1.1'
+
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-master_doc = 'index'
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinx.ext.mathjax]
 
 html_copy_source = True
 
@@ -38,10 +45,17 @@ markdown_extensions = ['gfm']  # Github Flavored Markdown (pip install py-gfm)
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+language = None
+
+pygments_style = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,8 +63,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    "navigation_depth": 2,
+    "collapse_navigation": False,
+    "navigation_depth": 3,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
