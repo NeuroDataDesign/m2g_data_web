aa9f6c96007e7496de2d728bf762676eff6c8a09
Fang Cai <fangcai@Fangs-MacBook-Pro.local>
2020-03-30 14:08:21 -0400

    conf.py chagnges

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ release = u'0.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinx.ext.mathjax]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinx.ext.mathjax']
 
 html_copy_source = True
 
